801640e121ce606615cf374122c2aef67ddcd1f0
Danylo <danyloboshagora@gmail.com>
2022-04-22 19:46:42 +0200

    init

---
+++ gymmobile/app/src/main/java/com/example/gym_mobile/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.gym_mobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
