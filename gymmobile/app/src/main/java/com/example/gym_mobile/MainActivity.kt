2c33949dfba1a30ec23c0aa27471ab362eeafb45
Montekar <mantas.pasakarnis@gmail.com>
2022-05-04 19:20:14 +0200

    navigation and search with mock data

--- gymmobile/app/src/main/java/com/example/gym_mobile/MainActivity.kt
+++ gymmobile/app/src/main/java/com/example/gym_mobile/MainActivity.kt
@@ -1,14 +1,27 @@
 package com.example.gym_mobile
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.example.gym_mobile.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController= Navigation.findNavController(this,R.id.activity_main_nav_host_fragment)
+        setupWithNavController(binding.bottomNavigationView,navController)
+
     }
 }
---
+++ gymmobile/app/src/main/java/com/example/gym_mobile/TrackingFragment.kt
@@ -0,0 +1,32 @@
+package com.example.gym_mobile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.gym_mobile.databinding.FragmentTrackingBinding
+
+
+class TrackingFragment : Fragment() {
+
+    private lateinit var binding: FragmentTrackingBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentTrackingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnGoToExercises.setOnClickListener {
+            findNavController().navigate(R.id.action_trackingFragment_to_exercisesFragment)
+        }
+    }
+
+}
