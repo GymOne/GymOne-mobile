e2cedda632d26435f176f53288c4d329daa291f3
Montekar <mantas.pasakarnis@gmail.com>
2022-05-08 12:13:10 +0200

    activity buttons

--- gymmobile/app/src/main/java/com/example/gym_mobile/LoginActivity.kt
+++ gymmobile/app/src/main/java/com/example/gym_mobile/LoginActivity.kt
@@ -1,11 +1,19 @@
 package com.example.gym_mobile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+        btnGoToRegister.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
     }
 }
