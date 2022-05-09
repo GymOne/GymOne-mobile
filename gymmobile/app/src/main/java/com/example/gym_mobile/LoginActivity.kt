0555b73b5a6e4b9d5ace06ca14c3bff5b19e7007
Danylo <danyloboshagora@gmail.com>
2022-05-09 17:51:52 +0200

    Nedas god fix

--- gymmobile/app/src/main/java/com/example/gym_mobile/LoginActivity.kt
+++ gymmobile/app/src/main/java/com/example/gym_mobile/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.gym_mobile.Services.AuthService
+import com.example.gym_mobile.Services.LoginDto
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -22,6 +23,7 @@ class LoginActivity : AppCompatActivity() {
 
     private fun getConnectionAuth() {
         var auth = AuthService();
+        var mb = LoginDto("dfv", "djhfbj")
         auth.apiCall(this, welcomeText)
     }
 }
