6e6731245f613b79d2506a0d17a9ce93978846c3
Danylo <danyloboshagora@gmail.com>
2022-05-09 19:09:50 +0200

    Nedas god fix 2.0

--- gymmobile/app/src/main/java/com/example/gym_mobile/AddSetFragment.kt
+++ gymmobile/app/src/main/java/com/example/gym_mobile/AddSetFragment.kt
@@ -1,5 +1,7 @@
 package com.example.gym_mobile
 
+import android.app.AlertDialog
+import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
@@ -8,41 +10,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.DialogFragment
+import androidx.navigation.fragment.findNavController
+import com.example.gym_mobile.databinding.FragmentTrackingBinding
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import kotlinx.android.synthetic.main.fragment_add_set.*
+import kotlinx.android.synthetic.main.fragment_tracking.*
 
 class AddSetFragment : DialogFragment() {
-    // Use this instance of the interface to deliver action events
-    internal lateinit var listener: AddSetDialogListener
 
-    /* The activity that creates an instance of this dialog fragment must
-     * implement this interface in order to receive event callbacks.
-     * Each method passes the DialogFragment in case the host needs to query it. */
-    interface AddSetDialogListener {
-        fun onDialogPositiveClick(dialog: DialogFragment){
-            Log.d("LOG", "positive click")
-        }
-        fun onDialogNegativeClick(dialog: DialogFragment) {
-            Log.d("LOG", "negative click")
-        }
-    }
-
-    // Override the Fragment.onAttach() method to instantiate the NoticeDialogListener
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        // Verify that the host activity implements the callback interface
-
-//        saveSet.setOnClickListener(){
-//            Log.d("LOG", "trying to save set")
-//
-//        }
-        try {
-            // Instantiate the NoticeDialogListener so we can send events to the host
-            listener = context as AddSetDialogListener
-        } catch (e: ClassCastException) {
-            // The activity doesn't implement the interface, throw exception
-            throw ClassCastException((context.toString() +
-                    " must implement DialogListener"))
-        }
-    }
 }
