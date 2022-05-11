38ab594f1e6822f2aeaa110cb84d4a53141c88f2
Danylo <danyloboshagora@gmail.com>
2022-05-11 17:31:19 +0200

    Booooooom login works and register too but still some small

--- gymmobile/app/src/main/java/com/example/gym_mobile/Services/AuthService.kt
+++ gymmobile/app/src/main/java/com/example/gym_mobile/Services/AuthService.kt
@@ -1,14 +1,10 @@
 package com.example.gym_mobile.Services
-import android.util.Log
-import android.widget.TextView
 import com.android.volley.Request
-import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.gym_mobile.LoginActivity
-import com.google.android.material.textfield.TextInputLayout
+import com.example.gym_mobile.RegisterActivity
 import org.json.JSONArray
 import org.json.JSONException
 import org.json.JSONObject
@@ -16,26 +12,19 @@ import org.json.JSONObject
 class AuthService {
     //val url = "https://pastebin.com/raw/2bW31yqa"
     val url = "http://10.0.2.2:3000/friend/getRequestsByEmail/sender@g.com"
+    val loginUrl = "http://10.0.2.2:3000/auth/login"
+    val registerUrl = "http://10.0.2.2:3000/auth/register"
 
-    fun apiCall(mainActivity: LoginActivity, textView: TextView) {
-
-
-
-
-
+    fun apiCall(mainActivity: LoginActivity, loginDto: LoginDto) {
 
         val request = JsonArrayRequest(
-            Request.Method.GET, // method
-            url, // url
+            Request.Method.POST, // method
+            loginUrl, // url
             null, // json request
             { response -> // response listener
-
                 try {
                     println(response)
                     val obj: JSONArray = response
-
-                    textView.text = ""
-
                     // loop through the array elements
                     for (i in 0 until  obj.length()){
                         // get current json object as student instance
@@ -47,17 +36,15 @@ class AuthService {
                         val receiver: String = student.getString("receiverId")
 
                         //display the formatted json data in text view
-                        textView.append("$id $sender\nage : $receiver\n\n")
                     }
 
                 }catch (e: JSONException){
-                    textView.text = e.message
+                    println(e)
                 }
 
             },
             { error -> // error listener
                 println(error)
-                textView.text = error.message
             }
         )
 
@@ -95,6 +82,73 @@ class AuthService {
 //        },
 //        Response.ErrorListener { welcomeText.text = "That didn't work!" })
         //que.add(stringRequest)
+    }
+
+    fun login(activity: LoginActivity, loginDto: LoginDto) : String{
+        //val json: String = Gson().toJson(loginDto)
+        val jsonBody = JSONObject()
+        jsonBody.put("email", loginDto.email)
+        jsonBody.put("password", loginDto.password)
+
+
+
+        val request = JsonObjectRequest(Request.Method.POST,loginUrl,jsonBody,
+            { response ->
+                // Process the json
+                try {
+                    activity.openMainActivity(NotProud(response))
+                    //var responseObject =  JSON.parse
+                }catch (e:Exception){
+                    println(e)
+                }
+
+            }, {
+                // Error in request
+                println("Volley error: $it")
+            })
+
+        println("Before requestQue")
+        val que = Volley.newRequestQueue(activity)
+        que.add(request)
+        return "test"
+        //val request: JsonObjectRequest = JsonObjectRequest(Request.Method.POST ,loginUrl, jsonBody, )
+    }
+
+    private fun NotProud(response: JSONObject?) : String {
+        var list = response.toString().split(":")
+        var tokenWithQuotation = list.get(1)
+        var finalToken: String  = tokenWithQuotation.substring(1, tokenWithQuotation.length - 1).dropLast(1);
+        return finalToken
+    }
+
+    fun register(registerDto: LoginDto, activity: RegisterActivity){
+        println("In auth service")
+        val jsonBody = JSONObject()
+        jsonBody.put("name", registerDto.name)
+        jsonBody.put("email", registerDto.email)
+        jsonBody.put("password", registerDto.password)
+
+
+
+        val request = JsonObjectRequest(Request.Method.POST,registerUrl,jsonBody,
+            { response ->
+                // Process the json
+                try {
+                    println(response)
+                    //activity.openMainActivity(NotProud(response))
+                    //var responseObject =  JSON.parse
+                }catch (e:Exception){
+                    println(e)
+                }
+
+            }, {
+                // Error in request
+                println("Volley error: $it")
+            })
+
+        println("Before requestQue")
+        val que = Volley.newRequestQueue(activity)
+        que.add(request)
     }
 
 }
