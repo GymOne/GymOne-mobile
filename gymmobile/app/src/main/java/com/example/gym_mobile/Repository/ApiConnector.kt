183b9cf9310babc827ff49c18089ce91e666af0c
Faustas Anulis <45820787+faustis1337@users.noreply.github.com>
2022-05-26 22:35:54 +0200

    btn and repo ip change

--- gymmobile/app/src/main/java/com/example/gym_mobile/Repository/ApiConnector.kt
+++ gymmobile/app/src/main/java/com/example/gym_mobile/Repository/ApiConnector.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiConnector {
-    val baseUrl = "http://10.0.2.2:3000/"
+    val baseUrl = "http://185.51.76.122:9091/" //LOCAL IP: http://10.0.2.2:3000/
 
     fun getInstance(): Retrofit {
         return Retrofit.Builder().baseUrl(baseUrl)
